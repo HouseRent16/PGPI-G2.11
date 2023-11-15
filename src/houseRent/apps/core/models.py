47d70a95ec02259f283573e6a6992357a2f8227b
Carmen-RuizPorcel <carruipor@alum.us.es>
2023-11-16 00:39:17 +0100

    HOUS-38: Feat Add Service entity

--- src/houseRent/apps/core/models.py
+++ src/houseRent/apps/core/models.py
@@ -47,6 +47,17 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return f"{self.street} {self.number}, {self.city}, {self.province}, {self.country}"
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=544)
+
+    class Meta:
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+
+    def __str__(self):
+        return f"{self.name} - {self.description}"
+
 
 class Accommodation(models.Model):
     name = models.CharField(max_length=200)
@@ -55,6 +66,7 @@ class Accommodation(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     category = models.CharField(max_length=10, choices=Category.choices())
+    service = models.ManyToManyField(Service)
 
     class Meta:
         verbose_name = "Alojamiento"
@@ -91,7 +103,10 @@ class Claim(models.Model):
         verbose_name_plural = "Reclamaciones"
 
     def __str__(self):
-        return f"{self.accommodation.name} - {self.title}"
+        return f"{self.accommodation.name} : {self.title}"
+
+
+
 
 
 
