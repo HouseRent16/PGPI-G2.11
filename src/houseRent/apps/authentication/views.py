7aa741c954ba38fe31fb54f78b1bdd6e15935042
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2023-11-15 10:40:46 +0100

    fix manage.py and templates folder

--- src/houseRent/apps/authentication/views.py
+++ src/houseRent/apps/authentication/views.py
@@ -23,4 +23,4 @@ def login_view(request):
     else:
         form = LoginForm()
 
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'authentication/login.html', {'form': form})
--- src/houseRent/houseRent/settings.py
+++ src/houseRent/houseRent/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "houseRent.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-ES"
 
 TIME_ZONE = "UTC"
 
