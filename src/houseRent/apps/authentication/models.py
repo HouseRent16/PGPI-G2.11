a10f1d1b0dd92c04f6f59a344523df7fe4ce5e27
marvicmar <marvicmar@alum.us.es>
2023-11-15 18:21:20 +0100

    HOUS-48: Add feat register
    Add register fuction, but it doesn't catch the route. Not tested.

--- src/houseRent/apps/authentication/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
-from django.core.validators import RegexValidator
-from apps.core.models import Address
-
-# Create your models here.
--- src/houseRent/apps/authentication/urls.py
+++ src/houseRent/apps/authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from apps.authentication import views
 
 urlpatterns = [
-    path('', views.login_view, name='login')
+    path('', views.login_view, name='login'),
+    path('register/', views.register, name='register')
 ]
