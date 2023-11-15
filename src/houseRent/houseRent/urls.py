bd79f234559c0c6f2b0197d6b34aae2a77022edd
marvicmar <marvicmar@alum.us.es>
2023-11-15 19:07:08 +0100

    HOUS-48: Fix login and register urls

--- src/houseRent/houseRent/urls.py
+++ src/houseRent/houseRent/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("apps.core.urls")),
     path("admin/", admin.site.urls),
-    path('login/', include("apps.authentication.urls"), name='login'),
+    path("", include("apps.authentication.urls")),
 ]
+
