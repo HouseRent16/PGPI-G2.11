83f62fbd605258759030a7fb7e3921ec49184b68
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-11-20 16:29:28 +0100

    HOUS-51 email test

--- src/houseRent/utils/tests.py
+++ src/houseRent/utils/tests.py
@@ -6,12 +6,12 @@ class MailerTestCase(TestCase):
 
     def test_enviar_correo(self):
         # Ejecuta la función que envía el correo
-        send_mail('destinatario@example.com', 'Asunto de prueba', 'Este es un mensaje de prueba.')
+        send_mail('Asunto de prueba', 'Cuerpo de prueba', ['sergiosantiago0403@gmail.com'])
 
         # Verifica que un correo se haya enviado
         self.assertEqual(len(mail.outbox), 1)
 
         # Verifica el contenido del correo
         self.assertEqual(mail.outbox[0].subject, 'Asunto de prueba')
-        self.assertEqual(mail.outbox[0].body, 'Este es un mensaje de prueba.')
-        self.assertEqual(mail.outbox[0].to, ['destinatario@example.com'])
+        self.assertEqual(mail.outbox[0].content_subtype, 'html')
+        self.assertEqual(mail.outbox[0].to, ['sergiosantiago0403@gmail.com'])
