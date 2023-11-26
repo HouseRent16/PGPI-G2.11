2685c12c1c29892e96de5892f905699e4bdcf8bf
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-11-26 22:52:28 +0100

    HOUS-60 booking process

--- src/houseRent/apps/booking/forms.py
+++ src/houseRent/apps/booking/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from apps.core.models  import Book, CustomUser
 from utils.validators import Validators
-from django.utils import timezone 
+from django.db.models import Q
+from django.utils import timezone
+
 
 
 class BookingRequest(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Oculta el campo de elección de la casa
+        self.fields['accommodation'].widget = forms.HiddenInput()
+
     class Meta:
         model=Book
-        fields=['start_date','end_date','payment_method','amount_people','special_requests']
+        fields=['start_date','end_date','payment_method','amount_people','special_requests', 'accommodation']
 
         widgets = {
             'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
@@ -28,9 +37,7 @@ class BookingRequest(forms.ModelForm):
         
     def clean_start_date(self):
         start_date = self.cleaned_data['start_date']
-        print('entra')
-        print(start_date)
-        #Validators.validate_future_datetime(start_date)
+        Validators.validate_future_datetime(start_date)
         return start_date
     
     def clean_amount_people(self):
@@ -40,26 +47,34 @@ class BookingRequest(forms.ModelForm):
          return amount_people
     
     def clean_end_date(self):
-        start_date = self.cleaned_data['start_date']
-        end_date = self.cleaned_data['end_date']
-        print('entra')
-        print(start_date, end_date)
+        start_date = self.cleaned_data.get('start_date')  # Usa get() para evitar KeyError
+        end_date = self.cleaned_data.get('end_date')
+
         if start_date and end_date and start_date > end_date:
-                print('error')
                 raise forms.ValidationError('La fecha de fin debe ser posterior a la de inicio')
         Validators.validate_future_datetime(end_date)
-        Validators.validate_dates(start_date, end_date)
-
         return end_date
     
+    def clean_overlapping(self):
+        start_date = self.cleaned_data['start_date']
+        end_date = self.cleaned_data['end_date']
+        accommodation = self.cleaned_data.get('accommodation')
+
+        if start_date and end_date:
+            overlapping_booking = Book.objects.filter(
+                  Q(accommodation=accommodation) &
+                  (Q(end_date__gt=start_date, start_date__lt=end_date) | Q(end_date__lt=end_date, end_date__gt=start_date) | Q(start_date__gt=start_date, start_date__lt=end_date))
+            )
+            if overlapping_booking.exists():
+                self.add_error('end_date', 'No hay disponibilidad del alojamiento seleccionado en las fechas especificadas.')
 
 class UserBookRequest2(forms.Form):
     email = forms.EmailField(label='Email')
     first_name = forms.CharField(label='Nombre')
     last_name = forms.CharField(label='Apellido')
     dni = forms.CharField(label='DNI')
-    birth_date = forms.DateField(label='Fecha de Nacimiento', widget=forms.DateInput(attrs={'type': 'date','class': 'form-control'}))
-
+    birth_date = forms.DateField(label='Fecha de Nacimiento', widget=forms.DateInput(attrs={'type': 'date','class': 'form-control', 'type': 'date'})),
+                                                                                            
 class UserBookRequest(forms.ModelForm):
     class Meta:
         model = CustomUser
--- src/houseRent/apps/booking/views.py
+++ src/houseRent/apps/booking/views.py
@@ -1,27 +1,47 @@
 from django.shortcuts import render, redirect
 from apps.core.models  import Accommodation
 from django.shortcuts import get_object_or_404
-from .forms import BookingRequest, UserBookRequest
+from .forms import BookingRequest, UserBookRequest2
 from django.forms.models import model_to_dict
 
 # Create your views here.
 
 
-def request_booking(request):
-    #accommodation = get_object_or_404(Accommodation, pk=accommodation_id)
+def request_booking(request, accommodation_id):
+    accommodation = get_object_or_404(Accommodation, pk=accommodation_id)
     current_user = request.user
     user_dict = model_to_dict(current_user)
     if not current_user.is_authenticated:
-        user_form = UserBookRequest()
+        user_form = UserBookRequest2()
     else:
-        user_form = UserBookRequest(instance=current_user)
+        user_form = UserBookRequest2(user_dict)
     if request.method == 'GET':
-        form = BookingRequest()
-        return render(request, 'booking/book.html', {'form': form, 'current_user': current_user, 'user_form': user_form})
+        form = BookingRequest(initial={'accommodation': accommodation})
+        form.accommodation = accommodation
+        return render(request, 'booking/book.html', {'form': form, 'current_user': current_user, 'user_form': user_form, "accommodation":accommodation})
     else:
         form = BookingRequest(request.POST)
-        if form.is_valid():
-            #Proyect.objects.create(name=request.POST['name'], description=request.POST['description'])
-            return redirect('/home')
+        form.accommodation = accommodation
+        user_form = UserBookRequest2(request.POST)
+        if not user_form.is_valid():
+            print('userform nor valid')
+            errores = user_form.errors.as_data()
+            # Imprime los errores a la consola para propósitos de depuración
+            print(errores)
+        if not form.is_valid():
+            print('form nor valid')
+
+
+        if form.is_valid() and user_form.is_valid():
+
+            booking_request = form.save(commit=False)
+            #user_loaded = user_form.save()
+            booking_request.user = current_user
+            booking_request.is_active = True
+            booking_request.accommodation = accommodation
+            booking_request.save()
+            return redirect('/')
         else: 
-            return render(request, 'booking/book.html', {'form': form, 'current_user': current_user, 'user_form': user_form})
+            
+            print("ha habido un error en el form")
+            return render(request, 'booking/book.html', {'form': form, 'current_user': current_user, 'user_form': user_form,  "accommodation":accommodation})
