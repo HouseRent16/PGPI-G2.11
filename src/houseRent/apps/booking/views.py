7d9e5ee05dd0a587e1516f4bf1d8f416dbeb5da4
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-11-27 23:51:21 +0100

    HOUS-60 Accommodation validation added

--- src/houseRent/apps/booking/views.py
+++ src/houseRent/apps/booking/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from apps.core.models  import Accommodation
+from apps.core.models  import Accommodation, CustomUser
 from django.shortcuts import get_object_or_404
 from .forms import BookingRequest, UserBookRequest
 from django.forms.models import model_to_dict
+from django.db.models import Q
 
 # Create your views here.
 
@@ -10,11 +11,9 @@ from django.forms.models import model_to_dict
 def request_booking(request, accommodation_id):
     accommodation = get_object_or_404(Accommodation, pk=accommodation_id)
     current_user = request.user
-    
     if not current_user.is_authenticated:
         user_form = UserBookRequest()
     else:
-        user_dict = model_to_dict(current_user)
         user_form = UserBookRequest(instance=current_user)
 
 
@@ -23,10 +22,13 @@ def request_booking(request, accommodation_id):
         form.accommodation = accommodation
         return render(request, 'booking/book.html', {'form': form, 'user_form': user_form, "accommodation":accommodation})
     else:
-        form = BookingRequest(request.POST)
+        form = BookingRequest(request.POST, initial={'accommodation': accommodation})
         form.accommodation = accommodation
         if not current_user.is_authenticated:
             user_form = UserBookRequest(request.POST)
+            if CustomUser.objects.filter(email=user_form.data.get('email')).exists():
+                current_user = CustomUser.objects.filter(Q(email=user_form.data.get('email'))).first()
+                user_form = UserBookRequest(request.POST, instance=current_user)
         else:
             user_form = UserBookRequest(request.POST, instance=current_user)
 
