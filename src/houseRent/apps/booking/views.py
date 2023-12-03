dbb89e902dbbb9073ca131f4fe7565fdb6d14308
“sergiosant02” <sergiosantiago0403@gmail.com>
2023-12-03 12:10:46 +0100

    HOUS-68 email with code to follow booking

--- src/houseRent/apps/booking/views.py
+++ src/houseRent/apps/booking/views.py
@@ -5,6 +5,9 @@ from .forms import BookingRequest, UserBookRequest
 from apps.core.enums import BookingStatus
 from django.forms.models import model_to_dict
 from django.contrib.auth.decorators import login_required
+from utils.mailer import send_mail
+from datetime import datetime
+
 from django.db.models import Q
 from apps.core.models import Book, Image
 from apps.core.enums import BookingStatus
@@ -128,6 +131,12 @@ def request_booking(request, accommodation_id):
             booking_request.accommodation = accommodation
             booking_request.status = BookingStatus.PENDING
             booking_request.save()
+            str_start_date = booking_request.start_date.strftime("%d/%m/%Y")
+            str_end_date = booking_request.end_date.strftime("%d/%m/%Y")
+            nights = (booking_request.end_date - booking_request.start_date)
+            price = (nights.days - 1 )* accommodation.price
+            body = "Su reserva para {} ha sido confirmada, para las fechas {} - {}. Por cun coste de {}€".format(accommodation.name, str_start_date, str_end_date, price)
+            send_mail("Información de reserva", body, [user_form.cleaned_data.get("email")],"mailer/email_booking.html", {"code": booking_request.code, "addres": accommodation.address})
             return redirect('/')
         else: 
             return render(request, 'booking/book.html', {'form': form, 'user_form': user_form,  "accommodation":accommodation})
