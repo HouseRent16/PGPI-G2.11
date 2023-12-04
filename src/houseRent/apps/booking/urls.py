89004a89475e52ead9fd4ae146d3be782c486eb5
marvicmar <marvicmar@alum.us.es>
2023-12-04 14:56:49 +0100

    Add register Image to accommodation

--- src/houseRent/apps/booking/urls.py
+++ src/houseRent/apps/booking/urls.py
@@ -5,7 +5,7 @@ from apps.booking import views
 urlpatterns = [
     path('', views.booking_details, name='booking'),
     path('<int:accommodation_id>', request_booking, name='request_booking'),
-    path('owner',views.books,name='booking'),
+    path('owner',views.books,name='gestion'),
     path('owner/<int:ID>',views.detailsBooks, name='detailsBooking'),
     path('history', booking_history, name='history'),
 ]
